cbd346d74b5e41700c294b10a339ee1fda7e3d47
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-02 19:14:45 +0100

    initialize browser project

---
+++ README.md
@@ -0,0 +1,4 @@
+cois-sprite-editor
+===========
+
+A means to store a sprite created with the sprite-editor
---
+++ cois-sprite-editor.js
@@ -0,0 +1,3 @@
+;(function(sprite, undefined){
+    /* A means to store a sprite created with the sprite-editor */
+})(window.sprite = window.sprite || {})
